f6b5ce55eb5813f19380a31349ffc995d37dbd8d
shubham <ruthsteve121@gmail,com>
2020-04-09 13:22:13 +0530

    deleted

--- Project Testing/routes/employerApiRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const controller = require('../controller/employerApiCon');
-const middleware = require('../middleware/employerAuth');
-const router = express.Router();
-
-// router.get('/employer_dashboard', middleware.authentication, controller.dashboardEmployer);
-router.post('/assignTask', middleware.authentication, controller.postDashEmployer);
-
-module.exports = router;
--- Project Testing/utilities/bufferToString.js
+++
@@ -1,9 +0,0 @@
-const Datauri = require("datauri")
-
-const datauri = new Datauri()
-
-
-module.exports = (filename, buffer) => {
-
-    return datauri.format(filename, buffer).content
-}
